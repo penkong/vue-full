0dc3e18ac99ae81cc4c5c647d80f2fc38620bea4
mazdak <nazemi.works@gmail.com>
2019-08-08 23:04:10 -0700

    usign assets as global and put related styles in it

---
+++ nuextapp/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header>
+      <ul>
+        <li>
+          <nuxt-link to="/users">HOme</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/home">USers</nuxt-link>
+        </li>
+      </ul>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ nuextapp/layouts/error.vue
@@ -0,0 +1,9 @@
+<template>
+  <!-- errror is reserved word by nuxt for layout for handle it -->
+  <div>
+    <h1>
+      an error occured; we are sorry:!
+      it come by itself.
+    </h1>
+  </div>
+</template>
